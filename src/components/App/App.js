dc2b70c01e3c0752d5e580b8ba2c53df3b523edd
kevinrosario <kevin.rosario9@upr.edu>
2019-07-22 09:55:22 -0400

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,61 @@
+import React, { Component, Fragment } from 'react'
+import { Route } from 'react-router-dom'
+
+import AuthenticatedRoute from '../AuthenticatedRoute/AuthenticatedRoute'
+import AutoDismissAlert from '../AutoDismissAlert/AutoDismissAlert'
+import Header from '../Header/Header'
+import SignUp from '../SignUp/SignUp'
+import SignIn from '../SignIn/SignIn'
+import SignOut from '../SignOut/SignOut'
+import ChangePassword from '../ChangePassword/ChangePassword'
+
+class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      user: null,
+      alerts: []
+    }
+  }
+
+  setUser = user => this.setState({ user })
+
+  clearUser = () => this.setState({ user: null })
+
+  alert = (message, type) => {
+    this.setState({ alerts: [...this.state.alerts, { message, type }] })
+  }
+
+  render () {
+    const { alerts, user } = this.state
+
+    return (
+      <Fragment>
+        <Header user={user} />
+        {alerts.map((alert, index) => (
+          <AutoDismissAlert
+            key={index}
+            alert={alert}
+          />
+        ))}
+        <main className="container">
+          <Route path='/sign-up' render={() => (
+            <SignUp alert={this.alert} setUser={this.setUser} />
+          )} />
+          <Route path='/sign-in' render={() => (
+            <SignIn alert={this.alert} setUser={this.setUser} />
+          )} />
+          <AuthenticatedRoute user={user} path='/sign-out' render={() => (
+            <SignOut alert={this.alert} clearUser={this.clearUser} user={user} />
+          )} />
+          <AuthenticatedRoute user={user} path='/change-password' render={() => (
+            <ChangePassword alert={this.alert} user={user} />
+          )} />
+        </main>
+      </Fragment>
+    )
+  }
+}
+
+export default App
